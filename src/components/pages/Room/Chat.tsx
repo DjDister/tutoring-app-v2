c2dd3cb2b79bb7a605dabe1d1acf13b134bdc13c
DjFIREED <porebskifilip@wp.pl>
2024-03-04 07:40:34 +0100

    Add chat minimize functionality

--- src/components/pages/Room/Chat.tsx
+++ src/components/pages/Room/Chat.tsx
@@ -1,36 +1,59 @@
 import { Input } from "@/components/ui/input";
 import useMessages from "@/hooks/useMessages";
 import React, { useState } from "react";
+import { ChevronRight, ChevronLeft } from "lucide-react";
 
 export default function Chat({ roomId }: { roomId: string }) {
   const [messageInput, setMessageInput] = useState("");
   const { messages, sendMessage } = useMessages(roomId);
+  const [isMinimized, setIsMinimized] = useState(false);
+
+  const minimizeChat = () => {
+    setIsMinimized((prev) => !prev);
+  };
 
   return (
-    <div className="w-auto flex flex-col justify-end p-1">
-      <div className="flex flex-col h-full w-full overflow-scroll p-1">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`leading-1.5 p-4 w-fit border-gray-200 text-sm font-normal py-2.5 text-gray-900 dark:text-white ${
-              message.userName === "You"
-                ? "bg-blue-400 text-right self-end rounded-s-xl rounded-b-xl"
-                : "bg-gray-100 rounded-e-xl rounded-es-xl "
-            }`}
-          >
-            {message.message}
-          </div>
-        ))}
-      </div>
-      <div className="flex">
-        <Input
-          placeholder="Enter message"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setMessageInput(e.target.value);
-          }}
-        />
-        <button onClick={() => sendMessage(messageInput)}>{">"}</button>
-      </div>
-    </div>
+    <>
+      <button
+        className="absolute top-2 z-10 bg-white rounded-md right-3"
+        onClick={minimizeChat}
+      >
+        {isMinimized ? <ChevronLeft /> : <ChevronRight />}
+      </button>
+      {!isMinimized && (
+        <div className="w-auto flex flex-col justify-end p-1 relative">
+          <>
+            <div className="flex flex-col-reverse h-full w-full overflow-auto p-1">
+              {[...messages].reverse().map((message, index) => (
+                <div
+                  key={index}
+                  className={`leading-1.5 p-4 w-fit border-gray-200 text-sm font-normal py-2.5 text-gray-900 dark:text-white ${
+                    message.userName === "You"
+                      ? "bg-blue-400 text-right self-end rounded-s-xl rounded-b-xl"
+                      : "bg-gray-100 rounded-e-xl rounded-es-xl "
+                  }`}
+                >
+                  {message.message}
+                </div>
+              ))}
+            </div>
+            <div className="flex">
+              <Input
+                placeholder="Enter message"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setMessageInput(e.target.value);
+                }}
+                onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                  if (e.key === "Enter") sendMessage(messageInput);
+                }}
+              />
+              <button onClick={() => sendMessage(messageInput)}>
+                <ChevronRight />
+              </button>
+            </div>
+          </>
+        </div>
+      )}
+    </>
   );
 }
