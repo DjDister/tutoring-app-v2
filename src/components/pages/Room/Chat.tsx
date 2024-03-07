c7541b51ba93f2f4de60678dfa74750f379ee1a5
DjFIREED <porebskifilip@wp.pl>
2024-03-07 09:37:28 +0100

    Refactor Chat component to handle sending messages

--- src/components/pages/Room/Chat.tsx
+++ src/components/pages/Room/Chat.tsx
@@ -12,6 +12,12 @@ export default function Chat({ roomId }: { roomId: string }) {
     setIsMinimized((prev) => !prev);
   };
 
+  const handleSendMessage = () => {
+    if (!messageInput) return;
+    sendMessage(messageInput);
+    setMessageInput(() => "");
+  };
+
   return (
     <>
       <button
@@ -21,13 +27,13 @@ export default function Chat({ roomId }: { roomId: string }) {
         {isMinimized ? <ChevronLeft /> : <ChevronRight />}
       </button>
       {!isMinimized && (
-        <div className="w-auto flex flex-col justify-end p-1 relative">
+        <div className="w-[250px] flex flex-col justify-end p-1 relative">
           <>
-            <div className="flex flex-col-reverse h-full w-full overflow-auto p-1">
+            <div className="flex flex-col-reverse gap-2 h-full w-full overflow-auto p-1">
               {[...messages].reverse().map((message, index) => (
                 <div
                   key={index}
-                  className={`leading-1.5 p-4 w-fit border-gray-200 text-sm font-normal py-2.5 text-gray-900 dark:text-white ${
+                  className={`flex flex-wrap text-wrap break-all max-w-full leading-1.5 p-4 w-fit border-gray-200 text-sm font-normal py-2.5 text-gray-900 dark:text-white ${
                     message.userName === "You"
                       ? "bg-blue-400 text-right self-end rounded-s-xl rounded-b-xl"
                       : "bg-gray-100 rounded-e-xl rounded-es-xl "
@@ -37,17 +43,18 @@ export default function Chat({ roomId }: { roomId: string }) {
                 </div>
               ))}
             </div>
-            <div className="flex">
+            <div className="flex mt-2">
               <Input
+                value={messageInput}
                 placeholder="Enter message"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                   setMessageInput(e.target.value);
                 }}
                 onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
-                  if (e.key === "Enter") sendMessage(messageInput);
+                  if (e.key === "Enter") handleSendMessage();
                 }}
               />
-              <button onClick={() => sendMessage(messageInput)}>
+              <button onClick={handleSendMessage}>
                 <ChevronRight />
               </button>
             </div>
