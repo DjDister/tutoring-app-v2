99153d8a60b2328abb1ffcebe6d30b2d6bbdb60e
Filip Porebski <porebskifilip@wp.pl>
2024-03-31 09:26:21 +0200

    Add UnknownAction to actionQueue

--- src/lib/middleware/actionQueueMiddleware.ts
+++ src/lib/middleware/actionQueueMiddleware.ts
@@ -1,9 +1,11 @@
 import { Middleware } from "redux";
 import { AppDispatch, RootState } from "../store";
+import { UnknownAction } from "@reduxjs/toolkit";
 
 const actionQueueMiddleware: Middleware<{}, RootState> =
   (storeApi) => (next) => (action) => {
     let actionQueue: ReturnType<AppDispatch>[] = [];
+    actionQueue.push(action as UnknownAction);
 
     if (actionQueue.length > 0) {
       const nextAction = actionQueue.shift();
